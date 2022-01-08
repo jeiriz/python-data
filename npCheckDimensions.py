5ce8fc0119540232322fc335acf6f3f1b3cdbc78
Juan <juancruzeiriz@gmail.com>
2022-01-08 18:03:50 -0300

    Agrego archivos donde se muestra el uso básico de Numpy

---
+++ npCheckDimensions.py
@@ -0,0 +1,9 @@
+# ndim attribute returns an integer that tells us how many dimensions the array have.
+import numpy as np
+import npCreateArrays as arrays
+
+print(arrays.arr0d.ndim) # 0
+
+print(arrays.array3d.ndim) # 1
+
+print(arrays.array2d.ndim) # 2
---
+++ npCreateArrays.py
@@ -0,0 +1,32 @@
+# Python library
+# Usefull for arrays, math, stadistic, algebra lineal
+# Its like a list but faster 
+# ndarray
+# locality of reference
+
+import numpy as np
+
+print(np.__version__)
+# 0 - D Array
+arr0d = np.array(32)
+print(arr0d)
+# 1 - D Arrays
+arr1d = np.array([1,2,3,4,5])
+arrTuple1d = np.array((1,2,3,4,5)) #same as arr
+print(type(arrTuple1d))
+print("array tuple 1d", arrTuple1d)
+print(type(arr1d))
+print("array 1d", arr1d)
+
+#2 - D Arrays
+array2d= np.array([[1,2,3],[4,5,6]])
+print("array 2d", array2d)
+
+# 3 - D Arrays
+array3d = np.array([[[1,2,3],[4,5,6]],[[7,8,9]]])
+print("array 3d",array3d)
+
+# Create array with the dimension especified NDMIN
+arr5d = np.array([1,2,[3],3,6,7], ndmin=5)
+print(arr5d)
+
